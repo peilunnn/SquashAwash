58a28b4b75aa3d84904959bc568943b461026865
unknown <peilunnn@gmail.com>
2020-06-30 12:53:25 +0800

    added unity squash game project

---
+++ Assets/scripts/ServeManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ServeType {
+	public float upForce;
+	public float hitForce;
+}
+
+public class ServeManager : MonoBehaviour {
+	public ServeType lob;
+	public ServeType hard;
+}
+
+
